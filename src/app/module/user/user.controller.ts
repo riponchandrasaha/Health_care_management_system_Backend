a88e3d164da7746341495f69f3becd2a691c8360
Ripon Chandra Saha <riponsaha199@gmail.com>
2026-03-10 13:41:01 +0600

    Completing Create Doctor And Doctor Specialty Model And Route, Controller, Service Business Logic With Transaction In User Service

---
+++ src/app/module/user/user.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import status from "http-status";
+
+import { sendResponse } from "../../shared/sendResponse";
+import { UserService } from "./user.service";
+import { catchAsync } from "../../shared/CatchAsync";
+
+const createDoctor = catchAsync(
+    async (req: Request, res: Response) => {
+        const payload = req.body;
+
+        const result = await UserService.createDoctor(payload);
+
+        sendResponse(res, {
+            httpStatusCode: status.CREATED,
+            success: true,
+            message: "Doctor registered successfully",
+            data: result,
+        })
+    }
+)
+
+export const UserController = {
+    createDoctor,
+};
