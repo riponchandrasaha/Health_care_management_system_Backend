df5efe576882af8dc1cea3b707cf29a99d638ac7
Ripon Chandra Saha <riponsaha199@gmail.com>
2026-03-11 19:05:51 +0600

    Create Authorization Middleware And Protecting Specialty Routes

--- src/app/module/specialty/specialty.route.ts
+++ src/app/module/specialty/specialty.route.ts
@@ -1,10 +1,14 @@
+
 import { Router } from "express";
+import { Role } from "../../../generated/prisma/enums";
+;
 import { SpecialtyController } from "./specialty.controller";
+import { checkAuth } from "../../middleware/checkAuth";
 
 const router = Router();
 
-router.post('/', SpecialtyController.createSpecialty);
+router.post('/', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), SpecialtyController.createSpecialty);
 router.get('/', SpecialtyController.getAllSpecialties);
-router.delete('/:id', SpecialtyController.deleteSpecialty);
+router.delete('/:id', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), SpecialtyController.deleteSpecialty);
 
 export const SpecialtyRoutes = router;
