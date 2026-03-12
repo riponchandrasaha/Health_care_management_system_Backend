8bbc6849c940d4ce97692033a7ae9283b1535ccf
Ripon Chandra Saha <riponsaha199@gmail.com>
2026-03-12 18:55:34 +0600

    Refresh Token Route added

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
-import { AuthController } from "./auth.controller";
-import { checkAuth } from "../../middleware/checkAuth";
 import { Role } from "../../../generated/prisma/enums";
+import { checkAuth } from "../../middleware/checkAuth";
+import { AuthController } from "./auth.controller";
 
 const router = Router()
 
 router.post("/register", AuthController.registerPatient)
 router.post("/login", AuthController.loginUser)
 router.get("/me", checkAuth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN), AuthController.getMe)
+router.post("/refresh-token", AuthController.getNewToken)
+
+
 
 export const AuthRoutes = router;
